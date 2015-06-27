2d1cebedfbab18884b7d25e6a265264eed6ae8d1
Mihail-K <Mihail.K@fimu.tk>
2015-06-27 00:49:57 -0400

    Added regression tests.

---
+++ source/dquery/tests/regression.d
@@ -0,0 +1,308 @@
+
+module dquery.tests.regression;
+
+import dquery.d;
+
+struct Attr
+{
+}
+
+struct Limit
+{
+	int value;
+}
+
+struct None
+{
+}
+
+struct Coord
+{
+
+	@Attr
+	@Limit(10)
+	int x;
+
+	@Attr
+	@Limit(10)
+	int y;
+
+	@Attr
+	@Limit(10)
+	int z;
+
+	this(int x, int y, int z)
+	{
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
+
+	int[3] getCoords()
+	{
+		return [x, y, z];
+	}
+}
+
+unittest
+{
+	auto query = query!Coord;
+
+	// Should not be empty.
+	static assert(!query.empty);
+
+	// Should have 5 elements.
+	static assert(query.length == 5);
+
+	auto fields = query.fields;
+
+	// Should match parent type.
+	static assert(is(fields.type == query.type));
+
+	// Should not be empty.
+	static assert(!fields.empty);
+
+	// Should have 3 elements.
+	static assert(fields.length == 3);
+
+	foreach(element; fields)
+	{
+		// Should match parent type.
+		static assert(is(element.type == fields.type));
+
+		// Should have attribute Attr.
+		static assert(element.hasAttribute!Attr);
+
+		// Should have attribute Limit.
+		static assert(element.hasAttribute!Limit);
+
+		// Should not have attribute None.
+		static assert(!element.hasAttribute!None);
+
+		// Should not have nonexistent elements.
+		static assert(element.attributes.allow!None.empty);
+
+		// Should not have forbidden elements.
+		static assert(element.attributes.forbid!(Attr, Limit).empty);
+
+		foreach(attribute; element.attributes.allow!Attr)
+		{
+			// Should be a type attribute.
+			static assert(attribute.isType);
+
+			// Should not be an expression attribute.
+			static assert(!attribute.isExpression);
+
+			// Should be type Attr.
+			static assert(attribute.isTypeOf!Attr);
+
+			// Should not be type None.
+			static assert(!attribute.isTypeOf!None);
+
+			// Should be assignable to Attr.
+			static assert(attribute.isTypeAssignableTo!Attr);
+
+			// Should not be assignable to None.
+			static assert(!attribute.isTypeAssignableTo!None);
+
+			// Should be assignable from Attr.
+			static assert(attribute.isTypeAssignableFrom!Attr);
+
+			// Should not be assignable from None.
+			static assert(!attribute.isTypeAssignableFrom!None);
+		}
+
+		foreach(attribute; element.attributes.allow!Limit)
+		{
+			// Should be a type attribute.
+			static assert(!attribute.isType);
+
+			// Should not be an expression attribute.
+			static assert(attribute.isExpression);
+
+			// Should be type Limit.
+			static assert(attribute.isTypeOf!Limit);
+
+			// Should not be type None.
+			static assert(!attribute.isTypeOf!None);
+
+			// Should be assignable to Limit.
+			static assert(attribute.isTypeAssignableTo!Limit);
+
+			// Should not be assignable to None.
+			static assert(!attribute.isTypeAssignableTo!None);
+
+			// Should be assignable from Limit.
+			static assert(attribute.isTypeAssignableFrom!Limit);
+
+			// Should not be assignable from None.
+			static assert(!attribute.isTypeAssignableFrom!None);
+		}
+
+		// Should have public protection.
+		static assert(element.protection == "public");
+
+		// Should be a field.
+		static assert(element.isField);
+
+		// Should be an int.
+		static assert(element.isTypeOf!int);
+
+		// Should be not a ulong.
+		static assert(!element.isTypeOf!ulong);
+
+		// Should be assignable to int.
+		static assert(element.isTypeAssignableTo!int);
+
+		// Should be assignable to ulong.
+		static assert(element.isTypeAssignableTo!ulong);
+
+		// Should be assignable from int.
+		static assert(element.isTypeAssignableFrom!int);
+
+		// Should not be assignable from ulong.
+		static assert(!element.isTypeAssignableFrom!ulong);
+
+		// Should not be a function.
+		static assert(!element.isFunction);
+
+		// Should compile but not have arity 0.
+		static assert(__traits(compiles, {
+			static assert(!element.isArity!0);
+		}));
+
+		// Should compile but not return an int.
+		static assert(__traits(compiles, {
+			static assert(!element.isReturnType!int);
+		}));
+
+		// Should not be an aggregate.
+		static assert(!element.isAggregate);
+
+		// Should not be a class.
+		static assert(!element.isClass);
+
+		// Should not be a struct.
+		static assert(!element.isStruct);
+	}
+
+	auto functions = query.functions;
+
+	// Should not be empty.
+	static assert(!functions.empty);
+
+	// Should have 2 elements.
+	static assert(functions.length == 2);
+
+	foreach(element; functions)
+	{
+		// Should match parent type.
+		static assert(is(element.type == fields.type));
+
+		// Should not have attribute Attr.
+		static assert(!element.hasAttribute!Attr);
+
+		// Should not have attribute Limit.
+		static assert(!element.hasAttribute!Limit);
+
+		// Should not have attribute None.
+		static assert(!element.hasAttribute!None);
+
+		// Should not have nonexistent elements.
+		static assert(element.attributes.allow!None.empty);
+
+		// Should not have forbidden elements.
+		static assert(element.attributes.forbid!(Attr, Limit).empty);
+
+		// Should have public protection.
+		static assert(element.protection == "public");
+
+		// Should not be a field.
+		static assert(!element.isField);
+
+		// Should be an int.
+		static assert(!element.isTypeOf!int);
+
+		// Should be not a ulong.
+		static assert(!element.isTypeOf!ulong);
+
+		// Should be assignable to int.
+		static assert(!element.isTypeAssignableTo!int);
+
+		// Should be assignable to ulong.
+		static assert(!element.isTypeAssignableTo!ulong);
+
+		// Should be assignable from int.
+		static assert(!element.isTypeAssignableFrom!int);
+
+		// Should not be assignable from ulong.
+		static assert(!element.isTypeAssignableFrom!ulong);
+
+		// Should be a function.
+		static assert(element.isFunction);
+
+		static if(element.name == "__ctor")
+		{
+			// Should compile and have arity 3.
+			static assert(__traits(compiles, {
+				static assert(!element.isArity!3);
+			}));
+		}
+		else static if(element.name == "getCoords")
+		{
+			// Should compile and have arity 0.
+			static assert(__traits(compiles, {
+				static assert(!element.isArity!0);
+			}));
+		}
+		else
+		{
+			// Should not have any other elements.
+			static assert(0, "Unexpected element " ~ element.name);
+		}
+
+		static if(element.name == "__ctor")
+		{
+			// Should compile but not return an int.
+			static assert(__traits(compiles, {
+				static assert(!element.isReturnType!int);
+			}));
+
+			// Should compile but and return a Coord.
+			static assert(__traits(compiles, {
+				static assert(element.isReturnType!Coord);
+			}));
+		}
+		else static if(element.name == "getCoords")
+		{
+			// Should compile but not return an int.
+			static assert(__traits(compiles, {
+				static assert(!element.isReturnType!int);
+			}));
+
+			// Should compile but and return an int array.
+			static assert(__traits(compiles, {
+				static assert(element.isReturnType!(int[3]));
+			}));
+		}
+		else
+		{
+			// Should not have any other elements.
+			static assert(0, "Unexpected element " ~ element.name);
+		}
+
+		// Should not be an aggregate.
+		static assert(!element.isAggregate);
+
+		// Should not be a class.
+		static assert(!element.isClass);
+
+		// Should not be a struct.
+		static assert(!element.isStruct);
+	}
+
+	auto aggregates = query.aggregates;
+
+	// Should be empty.
+	static assert(aggregates.empty);
+}
